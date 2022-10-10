9627be72d32378f887ef84ef7213065dc0a31866
Gautam Mittal <gmittal@expedia.com>
2022-10-10 23:46:02 +0530

    initialed project

---
+++ NewsApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApps
+//
+//  Created by Gautam Mittal on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
